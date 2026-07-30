9b89087d5a6dc4bc2266f9138d33ed2d695481a0
Maximasdasd <pokoratel228@gmail.com>
2026-07-30 21:41:39 +0300

    feat: добавить хеширование паролей и jwt

---
+++ src/bookingroom/core/security.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timedelta, timezone
+
+from pwdlib import PasswordHash
+import jwt
+
+from bookingroom.core.config import settings
+
+
+
+class SecurityService:
+    def __init__(self, 
+                 secret_key: str, 
+                 algorithm: str, 
+                 access_token_expire_minutes: int, 
+                 password_hasher: PasswordHash
+                ) -> None:
+
+        self.secret_key=secret_key
+        self.algorithm=algorithm
+        self.access_token_expire_minutes=access_token_expire_minutes
+        self.password_hasher=password_hasher
+
+    def verify_password(self, plain_password: str, hashed_password: str) -> bool:
+        """проверка хеша с паролем"""
+        return self.password_hasher.verify(plain_password, hashed_password)
+
+
+    def hash_password(self, password: str) -> str:
+        """получение хешированного пароля"""
+        return self.password_hasher.hash(password)
+
+
+    def create_access_token(self, staff_id: int) -> str:
+        """создание и получение токена jwt"""
+        payload = {
+            "sub": str(staff_id)
+        }
+        expire = datetime.now(timezone.utc) + timedelta(minutes=self.access_token_expire_minutes)
+        payload.update({"exp": expire})
+        encoded_jwt = jwt.encode(payload, self.secret_key, algorithm=self.algorithm)
+        return encoded_jwt
+
+
+    def decode_access_token(self, token: str) -> dict:
+        """декодирвание токена и получение claim"""
+        payload = jwt.decode(
+            token, 
+            self.secret_key, 
+            algorithms=[self.algorithm], 
+            options={"require": ["sub", "exp"]}
+        )
+        return payload
+
+
+security_service = SecurityService(
+    secret_key=settings.SECRET_KEY,
+    algorithm=settings.ALGORITHM,
+    access_token_expire_minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES,
+    password_hasher=PasswordHash.recommended()
+)
